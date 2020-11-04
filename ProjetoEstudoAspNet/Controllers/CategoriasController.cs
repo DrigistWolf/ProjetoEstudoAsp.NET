148f6af0fb7f1cae0122dafd952cc31eb37c6e01
Francine Hendz Monteiro <Fran_Monteiro96@hotmail.com>
2020-11-04 14:20:06 -0300

    Estudo até a pag 26 no cap 1.6

--- ProjetoEstudoAspNet/Controllers/CategoriasController.cs
+++ ProjetoEstudoAspNet/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoEstudoAspNet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,39 @@ namespace ProjetoEstudoAspNet.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static List<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() {
+                CategoriaId = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria() {
+                CategoriaId = 2,
+                Nome = "Monitores"
+            },
+            new Categoria() {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria() {
+                CategoriaId = 4,
+                Nome = "Mouses"
+            },
+            new Categoria() {
+                CategoriaId = 5,
+                Nome = "Desktops"
+            }
+        };
+
         // GET: Categorias
-        public ActionResult Index()
+        public ActionResult Create()
         {
             return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View(categorias);
         }
     }
 }
