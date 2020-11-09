f0ba524aedaeda10220ea25ab0cccf3a4f1e3b39
Francine Hendz Monteiro <Fran_Monteiro96@hotmail.com>
2020-11-09 10:58:02 -0300

    Criação da Classe Fabricante e sua implementação

---
+++ ProjetoEstudoAspNet/Controllers/FabricantesController.cs
@@ -0,0 +1,112 @@
+﻿using System.Web.Mvc;
+using ProjetoEstudoAspNet.Contexts;
+using System.Linq;
+using ProjetoEstudoAspNet.Models;
+using System.Net;
+using System.Data.Entity;
+
+namespace ProjetoEstudoAspNet.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new
+                HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(
+            c => c.Nome));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Fabricantes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+
+            
+        }
+
+        // GET: Testes/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.
+            Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: Fabricantes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricantes.
+            Find(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
